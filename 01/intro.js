1f5eea2d5670f7ec11f3422aee1b46504821887d
amankumar-aj <amankumar_aj@outlook.com>
2025-01-06 20:10:55 +0530

    01 updated into to js

---
+++ 01/intro.js
@@ -0,0 +1,46 @@
+document.write("This is my first Javascript code!")
+
+document.write('<h2>This is written inside JS using HTML tags</h2>');
+
+//JAVA SCRIPT COMMENTS - single line comments
+/*
+multi line
+comments
+*/
+document.write("JS Variables"+"<br>")
+var pi=3.14;
+var person="are you GOD";
+var answer='Yes I am!';
+document.write(pi + "<br>");
+document.write(person + "<br>");
+document.write(answer + "<br>");
+
+
+//button function 
+function myFunction()
+{
+    
+document.getElementById("demo").innerHTML="Hi I am JS";
+}
+
+
+var num = 10;
+console.log(num);
+console.log(typeof(num));
+num = false;
+console.log(num);
+console.log(typeof(num));
+num = "AMAN KUMAR JHA";
+console.log(num);
+console.log(typeof(num));
+num = null;
+console.log(num);
+console.log(typeof(num));
+num = undefined;
+console.log(num);
+console.log(typeof(num));
+
+
+
+
+
