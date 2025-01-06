76fc1802da41cbbff3f2c1dcce64385754c64b87
amankumar-aj <amankumar_aj@outlook.com>
2025-01-06 20:11:47 +0530

    02 updated datatypes in js

---
+++ 02/dataType.js
@@ -0,0 +1,59 @@
+
+var num = 10;
+document.write(num + "<br>");
+document.write(typeof(num) + "<br>");
+
+num = false;
+document.write(num + "<br>");
+document.write(typeof(num) + "<br>");
+
+num = "AMAN KUMAR JHA";
+document.write(num + "<br>");
+document.write(typeof(num) + "<br>");
+
+num = null;
+document.write(num + "<br>");
+document.write(typeof(num) + "<br>");
+
+num = undefined;
+document.write(num + "<br>");
+document.write(typeof(num) + "<br>");
+
+// string
+var companyName = 'Google';
+document.write(companyName + "<br>");
+
+// number
+var pi = 3.14;
+var year = 2013;
+document.write(pi + "<br>");
+document.write(year + "<br>");
+
+// boolean
+var flag = true;
+var FALSE = false;
+document.write(flag + "<br>");
+document.write(FALSE + "<br>");
+
+// function
+var sayHello = function () {
+    alert('hello world!');
+}
+sayHello();
+
+// array 
+var numberArray = [1, 2, 3];
+var animals = new Array("cat", "dog", "mouse", "lion");
+document.write(numberArray + "<br>");
+
+// object 
+var person = {
+    name: 'Barack Hussein Obama II',
+    age: '51',
+    title: '44th President of the United States'
+};
+document.write(person.name + "<br>");
+document.write(person.age + "<br>");
+document.write(person.title + "<br>");
+
+
